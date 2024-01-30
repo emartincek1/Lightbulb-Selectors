0b787fec0767b12e78b88c2d90e1b28b307a3689
emartincek1 <evan.martincek@gmail.com>
2024-01-30 09:23:56 -0600

    add selectors and event listeners for bulbs

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Write your code here
+const bulbs = document.querySelectorAll(".lightbulb");
+const clicked = document.querySelector(".subtitle");
+
+let clickedCount = 0;
+
+bulbs.forEach((bulb) => {
+  bulb.addEventListener("click", () => {
+    bulb.classList.toggle("active");
+    clickedCount++;
+    clicked.innerHTML = `You've clicked the lights ${clickedCount} times`;
+  });
+});
